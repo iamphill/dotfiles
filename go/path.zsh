5d3d570f3a4e5a00df730d2b6a85c97aed5d127e
Phil Hughes <me@iamphill.com>
2015-10-23 21:54:56 +0100

    added go & gvm

---
+++ go/path.zsh
@@ -0,0 +1,1 @@
+export PATH="$PATH:/Users/phil/.gvm/bin"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias reload!='. ~/.zshrc'
 
 alias hosts='vim /etc/hosts'
 alias vhosts='sudo vim /etc/apache2/extra/httpd-vhosts.conf'
+
+alias update='cd ~/.dotfiles && git pull'
