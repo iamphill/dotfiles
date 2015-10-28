545c6e6be9710075279de9c5ba387e00c2ce6bba
Phil Hughes <me@iamphill.com>
2015-10-28 16:14:26 +0000

    added hub

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,3 +4,13 @@ if test ! $(which brew)
 then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+if test $(which brew)
+then
+  if test ! $(which hub)
+  then
+    echo "==> Installing hub..."
+    brew install hub
+    echo "==> Installed hub!"
+  fi
+fi
