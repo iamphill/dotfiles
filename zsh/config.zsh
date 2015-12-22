2e6d463284c524db23529f3ada502d2b07c823e1
Phil Hughes <me@iamphill.com>
2015-12-22 11:01:27 +0000

    vim colours

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,3 +3,6 @@ ZSH_THEME="agnoster"
 DEFAULT_USER="phil"
 
 ENABLE_CORRECTION="true"
+
+BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
