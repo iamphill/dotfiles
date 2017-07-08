7601c1adca53d9504c3463bf31fc15a21f2e5dd9
Phil Hughes <me@iamphill.com>
2017-07-08 17:43:33 +0100

    restored terminal theme for now

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,7 +1,12 @@
-ZSH_THEME="iamphill"
+ZSH_THEME="avit"
 
 DEFAULT_USER="phil"
 
 ENABLE_CORRECTION="false"
 
 KEYTIMEOUT=1
+
+PROMPT='🚀 '
+
+BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
